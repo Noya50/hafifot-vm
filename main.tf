43ea5ec394fb5cdd6290305b1c3c31a1d90682aa
Noya50 <noyaleshem12@gmail.com>
2024-07-21 18:35:48 +0300

    (main.tf) fix source syntax

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "azurerm_network_interface" "this" {
 }
 
 module "diagnostic_setting" {
-  source = "git::https://github.com/Noya50/hafifot-diagnosticSetting.git"
+  source = "git::https://github.com/Noya50/hafifot-diagnosticSetting.git?ref=main"
 
   name                       = "${azurerm_network_interface.this.name}-diagnostic-setting"
   target_resource_id         = azurerm_network_interface.this.id
